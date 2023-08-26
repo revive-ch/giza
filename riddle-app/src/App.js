5cbf51451d8f7595874ed933aedc28214420851d
iwaderem <iwaderem@ncsu.edu>
2023-08-26 07:47:26 -0400

    Fixed type in App.js

--- riddle-app/src/App.js
+++ riddle-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   // Single riddle properties
   const question = "My very name is known nationwide, especially by all my fellow buckeyes. On this campus my name is spread, although most only recognize my head. Today I decided to take a seat, in the building where students, staff, and diners meet.";
-  const answer = 'Brutus';
+  const answer = 'brutus';
   const audioSrc = `${process.env.PUBLIC_URL}/audio/Union.m4a`;
 
   return (
